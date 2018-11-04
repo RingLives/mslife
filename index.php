80820f1b2aac41514bb64845885a40ea02d6de10
Shohidur Rahman <sohidurr49@gmail.com>
2018-11-04 20:29:43 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Mase Life</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <style type="text/css">
+    	body{
+    		background-color: #FAFAFA;
+    	}
+    </style>
+  </head>
+  <body>
+	<section>
+		<nav class="navbar navbar-default">
+		    <div class="col-sm-10 col-sm-offset-1">
+		    <div class="navbar-header">
+		        <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+		            <span class="sr-only">Toggle navigation</span>
+		            <span class="icon-bar"></span>
+		            <span class="icon-bar"></span>
+		            <span class="icon-bar"></span>
+		        </button>
+		        <a href="#" class="navbar-brand">MaseLife</a>
+		    </div>
+			    <div id="navbarCollapse" class="collapse navbar-collapse">
+			        <ul class="nav navbar-nav navbar-right">
+			            <li><a href="#">Sign up</a></li>
+			            <li><a href="#">Login</a></li>
+			        </ul>
+			    </div>
+		    </div>
+		</nav>
+	</section>
+  	<div class="container">
+  		<div class="row">
+  			<div class="col-sm-10 col-sm-offset-1">
+	  		<div class="panel panel-default">
+	  			<div class="panel-body">
+	  				<div class="col-sm-8">
+	  					<div class="panel panel-default">
+	  						<div class="panel-body">
+	  							<div class="form-group">
+	  								<label class="label-control col-sm-12">Email</label>
+	  								<div class="col-sm-12">
+	  									<input type="text" name="email" class="form-control">
+	  								</div>
+	  							</div>
+	  						</div>
+	  					</div>
+	  				</div>
+	  				<div class="col-sm-4">
+	  					<div class="panel panel-default">
+	  						<div class="panel-body">
+	  							as
+	  						</div>
+	  					</div>
+	  				</div>
+	  			</div>
+	  		</div>
+	  		</div>
+  		</div>
+  	</div>
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script> -->
+    <script src="js/jquery/jquery.js"></script>
+    <script src="js/bootstrap/bootstrap.min.js"></script>
+  </body>
+</html>
